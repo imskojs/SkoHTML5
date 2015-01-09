63b3bbc54036655a5d7a94b72c649fa3fa7e1a32
Seunghoon Ko <imskojs@gmail.com>
2015-01-09 11:06:27 +0900

    컴밋 1

---
+++ public/js/console.shim.js
@@ -0,0 +1,23 @@
+// 콘솔없는 브라우저에서 콘솔에러 없에기.
+(function() {
+    var method,
+        noop = function () {},
+        methods = [
+            'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+            'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+            'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+            'timeStamp', 'trace', 'warn'
+        ],
+        length = methods.length,
+        console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
